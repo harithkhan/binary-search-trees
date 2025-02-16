361d96bce57450181fde138755d2fa264cc0a4bd
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-16 15:17:34 +0800

    feat: add sorting to function buildTree

--- tree.js
+++ tree.js
@@ -8,6 +8,7 @@ export class Tree {
 }
 
 function buildTree(arr) {
+    arr.sort((a, b) => a - b);
     if (arr.length === 0) return null;
     const middleIndex = Math.floor((0 + arr.length - 1) / 2);
     const middleNode = new Node(arr[middleIndex]);
@@ -18,8 +19,21 @@ function buildTree(arr) {
     return middleNode;
 }
 
+// const prettyPrint = (node, prefix = "", isLeft = true) => {
+//     if (node === null) {
+//         return;
+//     }
+//     if (node.right !== null) {
+//         prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+//     }
+//     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+//     if (node.left !== null) {
+//         prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+//     }
+// };
+
 const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const testArr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const testArr2 = [1, 2, 3, 4, 5, 6, 9, 8, 7];
 const testTree = new Tree(testArr2);
 console.log(JSON.stringify(testTree, null, 2));
-
+// prettyPrint(testTree.value);
