84679235d000b02750d1e3c60a1b99e57aa06175
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-16 15:06:45 +0800

    feat: add function buildTree() in module tree.js

--- tree.js
+++ tree.js
@@ -1,6 +1,25 @@
+import { Node } from "./node.js";
+
 export class Tree {
     constructor(arr = []) {
         this.array = arr;
-        this.root = null;
+        this.root = buildTree(arr);
     }
 }
+
+function buildTree(arr) {
+    if (arr.length === 0) return null;
+    const middleIndex = Math.floor((0 + arr.length - 1) / 2);
+    const middleNode = new Node(arr[middleIndex]);
+    const leftArr = arr.slice(0, middleIndex);
+    const rightArr = arr.slice(middleIndex + 1);
+    middleNode.left = leftArr.length === 0 ? null : buildTree(leftArr);
+    middleNode.right = rightArr.length === 0 ? null : buildTree(rightArr);
+    return middleNode;
+}
+
+const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const testArr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const testTree = new Tree(testArr2);
+console.log(JSON.stringify(testTree, null, 2));
+
