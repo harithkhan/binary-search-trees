1fd7d0ef6bf05478bdf367487437ce5e38c266ca
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-16 15:21:54 +0800

    feat: add function prettyPrint to tree.js

--- tree.js
+++ tree.js
@@ -1,12 +1,5 @@
 import { Node } from "./node.js";
 
-export class Tree {
-    constructor(arr = []) {
-        this.array = arr;
-        this.root = buildTree(arr);
-    }
-}
-
 function buildTree(arr) {
     arr.sort((a, b) => a - b);
     if (arr.length === 0) return null;
@@ -19,21 +12,28 @@ function buildTree(arr) {
     return middleNode;
 }
 
-// const prettyPrint = (node, prefix = "", isLeft = true) => {
-//     if (node === null) {
-//         return;
-//     }
-//     if (node.right !== null) {
-//         prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-//     }
-//     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-//     if (node.left !== null) {
-//         prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-//     }
-// };
+export class Tree {
+    constructor(arr = []) {
+        this.array = arr;
+        this.root = buildTree(arr);
+    }
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+        return;
+    }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
 
 const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const testArr2 = [1, 2, 3, 4, 5, 6, 9, 8, 7];
 const testTree = new Tree(testArr2);
 console.log(JSON.stringify(testTree, null, 2));
-// prettyPrint(testTree.value);
+prettyPrint(testTree.root);
