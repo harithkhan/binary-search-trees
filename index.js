9dba49a64752ed22452ddc339b77b31c959a55a4
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-18 21:14:27 +0800

    feat: confirm that testTree is balanced in driver

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ import { Tree } from "./tree.js";
 import { prettyPrint } from "./pretty-print.js";
 
 console.log(
-    "1. Create a binary search tree from an array of random numbers < 100."
+    `\n1. Create a binary search tree from an array of random numbers < 100.`
 );
-const randomArray = Array.from({ length: 25 }, () =>
-    Math.floor(Math.random() * 100)
+const randomArray = Array.from(
+    { length: 25 },
+    () => 1 + Math.floor(Math.random() * 100)
 );
 const testTree = new Tree(randomArray);
 console.log(`\nNew Binary Search Tree:`)
 console.log(testTree);
 prettyPrint(testTree.root);
+
+console.log(`\n2. Confirm that the tree is balanced by calling isBalanced.`);
+console.log(`${testTree.isBalanced()}`);
