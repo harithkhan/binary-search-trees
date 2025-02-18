b2924e61e0670fb28f25cc050acc7837cbc94a58
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-18 21:09:16 +0800

    feat: add new binary search tree to driver script Add a binary search tree from an array of random numbers < 100.

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 import { Tree } from "./tree.js";
 import { prettyPrint } from "./pretty-print.js";
 
-const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const testTree = new Tree(testArr);
-console.log(JSON.stringify(testTree, null, 2));
-prettyPrint(testTree.root);
-
-testTree.insert(21);
-testTree.insert(22);
-console.log(JSON.stringify(testTree, null, 2));
-prettyPrint(testTree.root);
-console.log(testTree.isBalanced());
-testTree.rebalance();
-console.log(JSON.stringify(testTree, null, 2));
+console.log(
+    "1. Create a binary search tree from an array of random numbers < 100."
+);
+const randomArray = Array.from({ length: 25 }, () =>
+    Math.floor(Math.random() * 100)
+);
+const testTree = new Tree(randomArray);
+console.log(`\nNew Binary Search Tree:`)
+console.log(testTree);
 prettyPrint(testTree.root);
